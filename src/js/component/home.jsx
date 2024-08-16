fb5c0ccc36e04d627de7cde648267ea17cc639c1
Ricard Mesegué <ricardm1995@gmail.com>
2024-08-16 19:32:15 +0000

    feat: keep doing

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { NavBar } from "./NavBar.jsx";
+import { Jumbotron } from "./Jumbotron.jsx";
+import { Card } from "./Card.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<NavBar />
+			<Jumbotron />
+			<div className="card-container" style={{ display: 'flex', justifyContent: 'center', gap: '1rem', flexWrap: 'wrap' }}>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 			<p>
-				<img src={rigoImage} />
+				<img src="" alt="" />
 			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
